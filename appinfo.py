36543435fe6b144e0405c1a164650e4f6c1be3e8
Mushahid <mushahid.sharif@gmail.com>
2026-02-26 21:15:28 +0500

    Fix project name

--- appinfo.py
+++ appinfo.py
@@ -6,7 +6,7 @@ APP_BASE_DIR = str(Path(__file__).parent.resolve())
 
 EMAIL_TEMPLATES_DIR = APP_BASE_DIR +  "/sysdata/email_templates"
 
-APP_NAME = "DishTalk"
+APP_NAME = "BlogBlaze"
 
 templates = Jinja2Templates(directory="templates")
 app_global_data = {"APP_NAME":APP_NAME}
